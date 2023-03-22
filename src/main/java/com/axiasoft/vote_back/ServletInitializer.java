2031c991a88197e87551a3af90c08aabe5f9e9fc
kylee9612 <iiii410@naver.com>
2023-03-22 15:45:08 +0900

    [K] Vote_Back Init

---
+++ src/main/java/com/axiasoft/vote_back/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.axiasoft.vote_back;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VoteBackApplication.class);
+	}
+
+}
---
+++ src/main/java/com/axiasoft/vote_back/VoteBackApplication.java
@@ -0,0 +1,13 @@
+package com.axiasoft.vote_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteBackApplication.class, args);
+	}
+
+}
